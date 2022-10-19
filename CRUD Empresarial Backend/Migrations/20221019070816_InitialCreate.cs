08f39ec869e3f44df2320d7e57c31b377a1aebfc
Impalatore <naim7710@gmail.com>
2022-10-19 12:17:00 -0500

    Controladores Terminados

---
+++ CRUD Empresarial Backend/Migrations/20221019070816_InitialCreate.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRUDEmpresarialBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Departamentos",
+                columns: table => new
+                {
+                    departamentoID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    departamentoNombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    encargadoID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departamentos", x => x.departamentoID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Empleados",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nombres = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    apellidoPaterno = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    apellidoMaterno = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    departamentoID = table.Column<int>(type: "int", nullable: false),
+                    email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    telefono = table.Column<int>(type: "int", nullable: false),
+                    direccion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    fechaIngreso = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    archivoFoto = table.Column<byte>(type: "tinyint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Empleados", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Departamentos");
+
+            migrationBuilder.DropTable(
+                name: "Empleados");
+        }
+    }
+}
